17c390223b3b11badfb9ea6fe2723f558d1a799b
Hemasri Kottapalli <120649018+HemasriKottapalli@users.noreply.github.com>
2023-07-19 13:28:10 +0530

    Add files via upload
    day-1

---
+++ Bulb/script.js
@@ -0,0 +1,6 @@
+function bulbOff(){
+    document.getElementById("bulb").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvR520LeGXnVQH8Cr485rQpeHS-ZUYVnGAvw&usqp=CAU";
+}
+function bulbOn(){
+    document.getElementById("bulb").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvKm5xnTBSikuTroWgPFZ6J6kNwistShaA0A&usqp=CAU";
+}
