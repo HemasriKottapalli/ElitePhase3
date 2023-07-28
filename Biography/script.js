2780ad4894f0a7d074662f9a525fb93d18973848
Hemasri Kottapalli <120649018+HemasriKottapalli@users.noreply.github.com>
2023-07-28 22:01:06 +0530

    Add files via upload
    react

---
+++ Biography/script.js
@@ -0,0 +1,17 @@
+ReactDOM.render(
+    <div>
+    <h1>LOVE BABBAR BHAIYYA</h1>
+    <p>In my words, He's the best teacher I know and a beautiful human being.</p>
+    <img src="./lovebabbar.jpeg" alt="Love babbar bhaiyya's image"/>
+    <h2>Education</h2>
+    <hr/>
+    <p>Love Babbar was born and brought up in a middle-class family. He completed his schooling from Andhra Education Society School, Delhi with 95% in class 12th. During his class 11th, he used to take home tuitions for 1000 rupees per month. Thereafter he completed his engineering degree from the Netaji Subhas University of Technology, Delhi.</p>
+    <h2>Today</h2>
+    <hr/>
+    <p>His main YouTube channel  "Love Babbar" has 132K subscribers, where on one hand he is entertaining his audience with vlogs and on the other hand, he is empowering the students with extraordinary educational content. A few months back He also started his new YouTube channel "Babbar CODE HELP" to guide the coders. He still has a very long way to go and we wish him good luck. So if you haven't checked his channel yet then go and check it out right now.  </p>
+    <hr/>
+    <pre><q>"We only have one life and it's too short, it passes like a snap. So try to fulfill all your dreams, all your desires, goals, ambitions in this life only rather than living someone else's life."</q></pre>
+    <p>  -Love Babbar</p>
+    <hr/>
+</div>,
+document.getElementById('content'));
